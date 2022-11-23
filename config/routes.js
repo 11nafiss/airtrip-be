0063f6f655a3c26431ee3734f1851ebcdd2fb982
NoFall887 <naufalamiruddin123456@gmail.com>
2022-11-23 20:27:03 +0700

    project setup

---
+++ config/routes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const controllers = require("../app/controllers");
+
+const apiRouter = express.Router();
+
+// API List here
+// apiRouter.post("/api/login", controllers.api.v1.postController.create);
+
+module.exports = apiRouter;
