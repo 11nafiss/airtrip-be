079916f464112cf20d97bed15c3a843667078ccf
NoFall887 <naufalamiruddin123456@gmail.com>
2022-11-24 15:35:14 +0700

    write test for register repository

---
+++ app/repositories/usersRepository.js
@@ -0,0 +1,12 @@
+const { User } = require("../models");
+async function register(userData) {
+  try {
+    return await User.create({ userData, roleId: 2 });
+  } catch (error) {
+    return error;
+  }
+}
+
+module.exports = {
+  register,
+};
--- app/test/repositories/AuthenticationRepository.spec.js
+++ app/test/repositories/AuthenticationRepository.spec.js
@@ -1,5 +1,35 @@
-describe("AuthenticationRepository", () => {
-  describe("#register", () => {
-    it("should return created user model", () => {});
+const { User } = require("../../models");
+const userData = {
+  name: "user",
+  image: "userimg",
+  phone: "0812345678",
+  address: "Jl.kaki",
+  email: "email@email",
+};
+const user = new User({ ...userData, roleId: 2 });
+
+describe("UsersRepository", () => {
+  describe("register", () => {
+    beforeEach(() => {
+      jest.resetModules();
+    });
+
+    it("should return created user model", async () => {
+      const mockUserModel = {
+        create: jest.fn().mockReturnValue(Promise.resolve(user)),
+      };
+
+      jest.mock("../../models", () => {
+        return { User: mockUserModel };
+      });
+      const { User } = require("../../models");
+      const UsersRepository = require("../../repositories/usersRepository");
+      const res = await UsersRepository.register(userData);
+      expect(mockUserModel.create).toHaveBeenCalledWith({
+        userData,
+        roleId: 2,
+      });
+      expect(res).toBe(user);
+    });
   });
 });
