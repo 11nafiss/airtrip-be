b5215fdd0b1bcfe43378599288633705cb510378
NoFall887 <naufalamiruddin123456@gmail.com>
2022-11-24 20:53:14 +0700

    register test

---
+++ app/services/AuthenticationService.js
@@ -0,0 +1,9 @@
+const usersRepo = require("../repositories/usersRepository");
+
+async function register(userData) {
+  try {
+    return await usersRepo.register(userData);
+  } catch (error) {
+    return error;
+  }
+}
---
+++ app/test/services/AuthenticationService.spec.js
@@ -0,0 +1,34 @@
+const { User } = require("../../models");
+const userData = {
+  name: "user",
+  image: "userimg",
+  phone: "0812345678",
+  address: "Jl.kaki",
+  email: "email@email",
+};
+const user = new User({ userData, roleId: 2 });
+
+describe("AuthenticationService", () => {
+  describe("register", () => {
+    beforeEach(() => {
+      jest.resetModules();
+    });
+
+    it("should return registered user data", async () => {
+      const mockUserRepo = {
+        register: jest.fn().mockReturnValue(Promise.resolve(user)),
+      };
+
+      jest.mock("../../repositories/usersRepository", () => mockUserRepo);
+      const usersRepo = require("../../repositories/usersRepository");
+      const authenticationService = require("../../services/AuthenticationService");
+
+      const result = await authenticationService.register(userData);
+      expect(mockUserRepo.create).toHaveBeenCalledWith({
+        userData,
+        roleId: 2,
+      });
+      expect(result).toBe(user);
+    });
+  });
+});
