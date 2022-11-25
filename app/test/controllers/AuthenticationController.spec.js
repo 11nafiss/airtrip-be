6d4a646b3ddf48b2a532b805695cbd2e0f0099ae
NoFall887 <naufalamiruddin123456@gmail.com>
2022-11-25 12:25:47 +0700

    update auth controller unit test

--- app/test/controllers/AuthenticationController.spec.js
+++ app/test/controllers/AuthenticationController.spec.js
@@ -1,5 +1,6 @@
 const { User } = require("../../models");
 const bcryptjs = require("bcryptjs");
+const { EmailAlreadyRegisteredError } = require("../../errors");
 
 function encryptPass(password) {
   return bcryptjs.hashSync(password);
@@ -43,6 +44,30 @@ describe("AuthenticationController", () => {
       expect(mockAuthService.register).toHaveBeenCalledWith(mockRequest.body);
       expect(mockResponse.status).toHaveBeenCalledWith(201);
       expect(mockResponse.json).toHaveBeenCalledWith(user);
+    });
+
+    it("should call res.status(422) and res.json with email already registered error", async () => {
+      const mockRequest = {
+        body: {
+          email: "email@email",
+          password: "userpass",
+        },
+      };
+
+      const err = new EmailAlreadyRegisteredError(mockRequest.body.email);
+      const mockAuthService = {
+        register: jest.fn().mockReturnValue(Promise.resolve(err)),
+      };
+      // mock auth service
+      jest.mock("../../services/AuthenticationService", () => mockAuthService);
+
+      const authenticationService = require("../../services/AuthenticationService");
+      const { api } = require("../../controllers");
+      await api.v1.authenticationController.register(mockRequest, mockResponse);
+
+      expect(mockAuthService.register).toHaveBeenCalledWith(mockRequest.body);
+      expect(mockResponse.status).toHaveBeenCalledWith(422);
+      expect(mockResponse.json).toHaveBeenCalledWith(err);
     });
 
     it("should call res.status(500) ", () => {});
