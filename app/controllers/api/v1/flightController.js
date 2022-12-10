aa52c58df055071c6dfe302d7097a7f23f65e2b5
TrisnaWahyudi <wayantrisna79@gmail.com>
2022-12-10 11:13:50 +0800

    add 404 response to create flight route

--- app/controllers/api/v1/flightController.js
+++ app/controllers/api/v1/flightController.js
@@ -1,5 +1,8 @@
 const flightService = require("../../../services/flightService");
 const airportService = require("../../../services/airportService");
+const airplaneService = require("../../../services/airplaneService");
+
+const { RecordNotFoundError } = require("../../../errors");
 
 async function handleSearchFlights(req, res, next) {
   try {
@@ -18,49 +21,46 @@ async function handleSearchFlights(req, res, next) {
 
 async function handleCreateFlight(req, res, next) {
   try {
-    console.log("=== masuk try handleCreateFlight ===");
-    /*
-        req.body = {
-          departure,
-          arrival, 
-          class,
-          price,
-          from,
-          to,
-          airplane_id,
-          descripttion,
-        }
-    */
-
     // get the departure address
     const from = await airportService.getAirportById(req.body.from);
 
-    console.log("from", from);
-
     // add checking here
+    if (!from) {
+      const msg = `airport with id ${req.body.from}`;
+      const err = new RecordNotFoundError(msg);
+      res.status(404).json(err);
+      return;
+    }
 
     // add checking here
 
     // get the arrival address
     const to = await airportService.getAirportById(req.body.to);
 
-    console.log("to", to);
-
     // add checking here
-
-    // add checking here
+    if (!to) {
+      const msg = `airport with id ${req.body.to}`;
+      const err = new RecordNotFoundError(msg);
+      res.status(404).json(err);
+      return;
+    }
 
     // get the airplane data
+    const airplane = await airplaneService.getAirplaneById(req.body.airplane_id);
 
-    // const airplane = await
+    if (!airplane) {
+      const msg = `airplane with id ${req.body.airplane_id}`;
+      const err = new RecordNotFoundError(msg);
+      res.status(404).json(err);
+      return;
+    }
 
+    // write the data
     const flight = await flightService.createFlight(req.body);
-    console.log("flight", flight);
 
     // return the response
     res.status(201).json({ data: flight });
   } catch (error) {
-    console.log("=== masuk catch handleCreateFlight ===");
     res.status(422).json(error);
     next();
   }
